57c5990853bccac55baf808dd2521b720837d7d1
gelei <gelei@anve.com>
2020-05-12 18:09:05 +0800

    效果优化

--- GLEmitterDemo/ViewController.m
+++ GLEmitterDemo/ViewController.m
@@ -34,19 +34,31 @@
     NSArray *imgs = @[@"fengmi_SVG",@"huandengpian_SVG",@"mianbao_SVG",@"shala_SVG",@"shengyu_SVG",@"shuiguo_SVG",@"tang_SVG"];
     NSMutableArray *cells = [NSMutableArray array];
     for (int i = 0; i < imgs.count; i++) {
+        //发射单元
         CAEmitterCell *likeCell1 = [CAEmitterCell emitterCell];
-        //每s多少个
+        //每s多少个粒子
         likeCell1.birthRate = arc4random_uniform(3) + 8;
-        //存活时间
+        //单个存活时间
         likeCell1.lifetime = (arc4random_uniform(10) +40) * 0.01;
-        likeCell1.velocity = 800;
+        //单个粒子初始速度,
+        likeCell1.velocity = 1000;
+        //单个粒子初始速度变化范围
         likeCell1.velocityRange = 500;
-        likeCell1.yAcceleration = 300;
-        likeCell1.emissionLongitude = -M_PI_4;
+        //y轴加速度,重力效果,可以看到抛物线效果
+        likeCell1.yAcceleration = likeCell1.velocity * 2;
+        //x轴加速度
+        likeCell1.xAcceleration = -100;
+        //发射角度,x与y轴夹脚
+        likeCell1.emissionLongitude = -M_PI_4 * 1.5;
+        //发射角度变化范围,以发射角度为中心,左右扩散角度之和
         likeCell1.emissionRange = M_PI_2;
+        //发射角度,x与z轴夹脚
         likeCell1.emissionLatitude = 0;
+        //alpha可变化范围
         likeCell1.alphaRange = 0.f;
+        //生命周期内的alpha变化速度
         likeCell1.alphaSpeed = -1.f / likeCell1.lifetime;
+        //cell内容
         likeCell1.contents = (__bridge id _Nullable)([UIImage imageNamed:imgs[i]].CGImage);
         [cells addObject:likeCell1];
     }
